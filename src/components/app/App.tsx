e8e788ece428192f77380222df629e3057d39adb
Costele <costelerm@gmail.com>
2021-01-25 13:30:11 +0200

    Added Profile component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,9 +3,7 @@ import {Profile} from '../profile/Profile'
 
 export function App() {
   return (
-    <div className="bg-red-900 text-white justify-center">
-      Tailwind implementat
        <Profile></Profile>
-    </div>
+
   )
 }
